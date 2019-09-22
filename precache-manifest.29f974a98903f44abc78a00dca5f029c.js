3be6eaec21f7a2eddfc529216912bf078338efc7
abhishe3571 <abhishek3571@gmail.com>
2019-09-22 22:09:25 +0530

    Updates

---
+++ precache-manifest.29f974a98903f44abc78a00dca5f029c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c8edce8282851f600af67021152b8f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d15b65255672cfc6b777",
+    "url": "/monsters-rolodex/static/css/main.5fae8b99.chunk.css"
+  },
+  {
+    "revision": "5de856a0397e01400c8b",
+    "url": "/monsters-rolodex/static/js/2.20ee9e62.chunk.js"
+  },
+  {
+    "revision": "d15b65255672cfc6b777",
+    "url": "/monsters-rolodex/static/js/main.eecd27a5.chunk.js"
+  },
+  {
+    "revision": "9cc72822719977244c51",
+    "url": "/monsters-rolodex/static/js/runtime~main.87b7a984.js"
+  }
+]);
